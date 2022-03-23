eddeafc173744271bb04851ee215eec9570b7a2e
phunguyen25 <phunv@devcamp.edu.vn>
2022-03-23 10:14:46 +0700

    update App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-import { STAFFS } from "./Data/staffs.jsx";
-import EmployeeList from "./Components/EmployeeList";
-import { Navbar } from "reactstrap";
+import Main from "./Components/Main.js";
+import { BrowserRouter } from "react-router-dom";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      staffs: STAFFS,
-    };
-  }
   render() {
     return (
-      <div className="container">
-        <Navbar color="primary" expand="md" light>
-          Ứng dụng quản lý nhân sự v1.0
-        </Navbar>
-        <EmployeeList staffs={this.state.staffs} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
